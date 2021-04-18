722031bd117417f01572a275233d9cb00ee1c657
Thiru Thirunavukarasu <thiru0130@gmail.com>
2021-04-18 16:43:10 -0400

    Replaced hawk with beholder and refactored
    Hoping beholder is more reliable than hawk as it uses newer Java API's.

--- README.md
+++ README.md
@@ -2,32 +2,29 @@
 
 A simple library for hot reloading your Clojure code.
 
-Please note, **ClojureScript** is deliberately **unsupported**. [Figwheel](https://figwheel.org/) or [Shadow CLJS](http://shadow-cljs.org/) are probably your best options if you're working on a ClojurScript project. I personally only use this library for *ClojurScript-free* projects, and Figwheel-main otherwise.
+Please note, **ClojureScript** is deliberately **unsupported**. [Figwheel](https://figwheel.org/) or [Shadow CLJS](http://shadow-cljs.org/) are probably your best options if you're working on a ClojureScript project. I personally only use this library for ClojureScript-free projects, and Figwheel-main otherwise.
 
 ## Usage
 
 Add this to your **deps.edn**:
 
 ```clojure
-github-thiru/clj-reloader {:git/url "https://github.com/thiru/clj-reloader" :sha "INSERT SHA HERE"}
+thiru/clj-reloader {:git/url "https://github.com/thiru/clj-reloader"
+                    :sha "<COMMIT SHA>"}
 ```
-Example of starting the hot code reloader in **user.clj**:
+
+Example of starting the hot code reloader to monitor the *src* and *dev*
+directories:
 
 ```clojure
-(ns user
-  (:require [reloader.core :as reloader]))
+(require '[reloader.core :as reloader])
 
-(defonce hcr (reloader/start-watch ["src"]))
+(reloader/start ["src" "dev"])
 ```
+
 And if you need to stop the hot code reloader:
 
 ```clojure
-(reloader/stop-watch hcr)
+(reloader/stop)
 ```
-## Development
 
-While working on this project I like to launch a REPL with the following script, as it sets up a development-friendly environment the way I like (nREPL, Rebel-Readline). You might like it to:
-
-```shell
-$ bin/dev
-```
--- src/reloader/core.clj
+++ src/reloader/core.clj
@@ -1,107 +1,126 @@
 (ns reloader.core
-  "Sole namespace containing all library-specific logic."
+  "Sole namespace containing all functionality of this library."
   (:require ;; Clojure Core:
-            [clojure.data :as data]
-            [clojure.pprint :as pp]
             [clojure.string :as str]
 
             ;; Third-Party:
-            [hawk.core :as hawk]
-            [ns-tracker.core :as nst]))
+            [ns-tracker.core :as nst]
+            [nextjournal.beholder :as beholder]))
 
-(declare
-  reload
-  start-watch
-  stop-watch
-  watcher-filter)
+;; ## Vars
+;; ----------------------------------------------------------------------------
 
-;;; Primary API ---------------------------------------------------------------
+(declare -on-change)
 
-(defn start-watch
-  "Start watching the specified directories for file-system change events.
+(defonce
+  ^{:doc "Initially empty, but after `start` is called it contains a map with
+         the following keys:
+         * `:dirs`
+           * A vector of directories being monitored
+           * We store this primarily to be able to restart the watcher
+         * `:modified-namespaces`
+           * A function created by the `ns-tracker` library that returns the
+             list of namespaces that need to be reloaded
+           * See `nst/ns-tracker` for more info
+         * `:watcher`
+           * Contains an instance of the directory watcher
+           * We store this in order to stop/restart the watcher
+           * See `beholder/watch` for more info"}
+  state (atom {}))
 
-   * `dirs`
-     * A list of directory names
+;; ## Primary API
+;; ----------------------------------------------------------------------------
 
-   Returns a map with these keys:
-   * `:dirs`
-     * The directories beings watched
-   * `:ns-tracker-fn`
-     * A function, when called, returns a set of namespaces that need to be
-       reloaded
-   * `:watcher`
-     * The *hawk* file-system watcher object
-     * We need this to be able to stop the file-system watching
-       * e.g. via `stop-watch`"
+(defn start
+  "Start watching the given directories for file-system change events in order
+  to hot-reload code within the directories.
+
+  * `dirs`
+    * A list of directories (strings)
+
+  NOTE: if watching was previously started a new watch won't start without
+  first stopping the existing one (via `stop`).
+
+  Side-effects:
+  * Modifies `state`
+
+  Returns an instance of the watcher if successfull, otherwise `nil`."
   [dirs]
-  (if (empty? dirs)
-    (throw (ex-info "No watch directories provided" {})))
-  (let [ns-tracker-fn (nst/ns-tracker dirs)
-        cfg {:dirs dirs
-             :ns-tracker-fn ns-tracker-fn
-             :watcher (hawk/watch! [{:paths dirs
-                                     :filter watcher-filter
-                                     :handler (fn [ctx e]
-                                                (reload e ns-tracker-fn)
-                                                ctx)}])}]
-    (println
-      (pp/cl-format nil
-                    "Hot code reloader started watching ~A folder~:P: ~A"
-                    (count dirs)
-                    (str/join ", " dirs)))
-    cfg))
+  (cond
+    (empty? dirs)
+    (println "Not watching any directories as none were provided")
 
-(defn stop-watch
+    (not (empty? @state))
+    (println "Watcher already running. Please stop existing watcher first.")
+
+    :else
+    (let [modified-namespaces (nst/ns-tracker dirs)
+          watcher (apply beholder/watch -on-change dirs)]
+      (if (nil? watcher)
+        (println "Failed to start watcher on:" (str/join ", " dirs))
+        (do
+          (println "Started monitoring" (str/join ", " dirs)
+                   "for hot code reloading")
+          (reset! state {:dirs dirs
+                         :modified-namespaces modified-namespaces
+                         :watcher watcher})
+          watcher)))))
+
+(defn stop
   "Stop watching directories for changes.
 
-   * `watch-config`
-     * A map as specified as the return from `start-watch`"
-  [watch-config]
-  (when (and watch-config
-             (:watcher watch-config))
-    (hawk/stop! (:watcher watch-config))
-    (println
-      (pp/cl-format nil
-                    "Hot code reloader stopped watching ~A folder~:P: ~A"
-                    (count (:dirs watch-config))
-                    (str/join ", " (:dirs watch-config))))))
+  Side-effects:
+  * Modifies `state`
 
-;;; Primary API ===============================================================
+  Returns `true` if watching stopped successfully, otherwise `false`."
+  []
+  (if (empty? @state)
+    (do
+      (println "No file-system watcher currently running.")
+      false)
+    (do
+      (beholder/stop (:watcher @state))
+      (println "Stopped monitoring" (str/join ", " (:dirs @state))
+               "for hot code reloading")
+      (reset! state {})
+      true)))
 
-(defn reload
-  "Reload modified namespaces.
+(defn restart
+  "Restart current watcher.
 
-  * `fs-event`
-    * A map describing the file-system event that occurred"
-  [fs-event ns-tracker-fn]
-  (let [namespaces (ns-tracker-fn)]
-    (when (and (:file fs-event) (pos? (count namespaces)))
-      (println
-        (pp/cl-format nil
-                      "HCR: (~A) ~A (~A namespace~:P reloaded)"
-                      (str/upper-case (name (:kind fs-event)))
-                      (.getPath (:file fs-event))
-                      (count namespaces)))
-      (doseq [ns-sym namespaces]
-        ;(println (str "(require " ns-sym " :reload)")) ; DEBUG
-        (require ns-sym :reload)))))
+  Side-effects:
+  * Modifies `state`
 
-(defn watcher-filter
-  "Filter the type of file-system events we're interested in."
-  [_ctx {:keys [kind file]}]
-  (and file
-       ;; We're only interested in file modified events. I don't think we need
-       ;; to care about file deleted events as we can't unload the code anyway
-       ;; (as far as I know), and file created events don't seem significant.
-       (= :modify kind)
-       ;; Ignore directory events
-       (.isFile file)
-       (not (.isHidden file))
-       (let [file-name (.getName file)]
-         ;; Ignore hidden/temporary files
-         (and (not= \. (first file-name))
-              (not= \# (first file-name))
-              (not= \~ (last file-name))
-              ;; Only interested in Clojure file types
-              (or (str/ends-with? file-name "clj")
-                  (str/ends-with? file-name "cljc"))))))
+  Returns an instance of the watcher if successful, otherwise `nil`."
+  []
+  (if (empty? (:watcher @state))
+    (println "Not able to restart watcher as it was never originally started")
+    (let [curr-dirs (:dirs @state)]
+      (and (stop)
+           (start curr-dirs)))))
+
+;; ## Helpers
+;; ----------------------------------------------------------------------------
+
+(defn -on-change
+  "Handle a file-system change event (e.g. file was modified)."
+  [info]
+  (let [file-name (-> info :path .getFileName str (or ""))]
+    (when (and info
+               ;; We're only interested in modified events. Create events are
+               ;; superfluous as they're always immediately proceeded by a
+               ;; modified event. There's nothing we can (currently) do with
+               ;; delete events as unloading code isn't supported.
+               (= :modify (:type info))
+               (or (str/ends-with? file-name "clj")
+                   (str/ends-with? file-name "cljc")))
+      (let [ns-modified ((:modified-namespaces @state))
+            num-ns-modified (count ns-modified)]
+        (when (pos? num-ns-modified)
+          (doseq [ns-sym ns-modified]
+            (require ns-sym :reload))
+          (println (format "File '%s' was modified (%d namespace%s reloaded)"
+                           file-name
+                           num-ns-modified
+                           (if (= 1 num-ns-modified) "" "s"))))))))
+
